c212f2b019b60fa203f1c3bfa7b9295aeadf8bb8
yandrakarine <yandrakarine.dev@outlook.com>
2022-06-10 10:02:21 -0300

    correction func. close menu. ScrollReveal config

--- PetCare/frontend/main.js
+++ PetCare/frontend/main.js
@@ -5,9 +5,11 @@ function whenScroll() {
     if (scrollY) {
         navigation.classList.add('scroll')
         imgLogo.setAttribute('src', './assets/images/PetCare-logo-branco.png')
+
     } else {
         navigation.classList.remove('scroll')
         imgLogo.setAttribute('src', './assets/images/PetCare-logo.png')
+
     }
 
 }
@@ -19,5 +21,22 @@ function openMenu() {
 
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
-    imgLogo.setAttribute('src', './assets/images/PetCare-logo.png')
+    scrollY ? imgLogo.setAttribute('src', './assets/images/PetCare-logo-branco.png') : imgLogo.setAttribute('src', './assets/images/PetCare-logo.png')
+
 }
+
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`#home,
+ #home,
+ img,
+ #home .stats,
+ #services,
+ #services header,
+ #services .card,
+ #about,
+ #about header,
+ #about .content`)
